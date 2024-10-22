327ec283b5939d34e198158d41453b141a298c20
Pratistha Parajuli <parajulipratistha@gmail.com>
2024-10-22 16:48:25 +0545

    food
    made snake food

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import turtle
 import time
+import random
 
 delay = 0.1 
 
@@ -18,6 +19,14 @@ head.color("black")
 head.penup()
 head.goto(0,0)
 head.direction = "stop"
+
+#Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
 
 #Functions
 def go_up():
@@ -52,13 +61,19 @@ def move():
 #keyboard bindings
 wn.listen()
 wn.onkey(go_up, "w")
-wn.onkeypress(go_down, "d")
+wn.onkeypress(go_down, "s")
 wn.onkeypress(go_left, "a")
-wn.onkeypress(go_right, "s")
+wn.onkeypress(go_right, "d")
 
 #Main game loop
 while True:
     wn.update()
+
+    if head.distance(food) < 20:
+        #move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
 
     move()
     time.sleep(delay)
