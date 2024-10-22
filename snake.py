3daa6c12546ffb60fb3cbef84cf0a1a5a3da1706
Pratistha Parajuli <parajulipratistha@gmail.com>
2024-10-22 11:03:46 +0545

    move
    made snake head and added movement to it

--- snake.py
+++ snake.py
@@ -1,11 +1,66 @@
 import turtle
+import time
 
+delay = 0.1 
+
+#set up the screen
 wn = turtle.Screen()
-wn.title("SnaKe game ")
+wn.title("Snake game by Dazzle ")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
-wn.tracer(0)
+wn.tracer(0) 
 
+#Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
+#Functions
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_left():
+    head.direction = "left"
+
+def go_right():
+    head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+#keyboard bindings
+wn.listen()
+wn.onkey(go_up, "w")
+wn.onkeypress(go_down, "d")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "s")
+
+#Main game loop
+while True:
+    wn.update()
+
+    move()
+    time.sleep(delay)
 
 wn.mainloop()
