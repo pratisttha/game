2536c7050d965038a928bdc70a2a6ae6f26b30e8
Pratistha Parajuli <parajulipratistha@gmail.com>
2024-10-24 08:19:03 +0545

    collision
    Added collision with the border

--- snake.py
+++ snake.py
@@ -71,6 +71,21 @@ wn.onkeypress(go_right, "d")
 while True:
     wn.update()
 
+    #check for collision with the border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+         time.sleep(1)
+         head.goto(0,0)
+         head.direction = "stop"
+
+         #hide the segments
+         for segment in segments:
+             segment.goto(1000, 1000)
+
+         #clear the segments list
+         segments.clear()
+
+      
+
     #check for a collision with the food
     if head.distance(food) < 20:
         #move the food to a random spot
