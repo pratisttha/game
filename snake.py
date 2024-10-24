87782cb9b2b935991b08e1e70277a83d2fad020a
Pratistha Parajuli <parajulipratistha@gmail.com>
2024-10-24 08:33:21 +0545

    Body Collisions
    the body collides with itself and dies

--- snake.py
+++ snake.py
@@ -32,16 +32,20 @@ segments = []
 
 #Functions
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 def go_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
 def go_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 def move():
     if head.direction == "up":
@@ -77,13 +81,8 @@ while True:
          head.goto(0,0)
          head.direction = "stop"
 
-         #hide the segments
-         for segment in segments:
-             segment.goto(1000, 1000)
-
-         #clear the segments list
-         segments.clear()
-
+        
+        
       
 
     #check for a collision with the food
@@ -115,6 +114,24 @@ while True:
         
 
     move()
+
+    #check for head collisons with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            #hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            #clear the segments list
+            segments.clear()
+
+
+
+    
     time.sleep(delay)
 
 wn.mainloop()
